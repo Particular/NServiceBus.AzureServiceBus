04d28d2cd992836da1b6490267fd5ed46283cb52
Yves Goeleven <yves@goeleven.com>
2015-07-22 10:18:10 +0200

    renamed deterministic partition strategy to sharded

---
+++ src/Transport/Addressing/Partitioning/Strategies/ShardedNamespacePartitioningStrategy.cs
@@ -0,0 +1,37 @@
+namespace NServiceBus.AzureServiceBus.Addressing
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Configuration;
+
+    public class ShardedNamespacePartitioningStrategy : INamespacePartitioningStrategy
+    {
+        readonly IList<string> _connectionstrings;
+        Func<int> _shardingRule;
+
+        public ShardedNamespacePartitioningStrategy(IList<string> connectionstrings)
+        {
+            if (connectionstrings.Count <= 1)
+            {
+                throw new ConfigurationErrorsException("The 'Sharded' namespace partitioning strategy requires more than one namespace, please configure multiple azure servicebus namespaces");
+            }
+
+            _connectionstrings = connectionstrings;
+        }
+
+        public void SetShardingRule(Func<int> rule )
+        {
+            _shardingRule = rule;
+        }
+
+        public string GetConnectionString(string endpointname)
+        {
+            if (_shardingRule ==  null)
+            {
+                throw new ConfigurationErrorsException("The 'Sharded' namespace partitioning strategy requires a configured sharding rule to determine a namespace, please configure a sharding rule");
+            }
+
+            return _connectionstrings[_shardingRule()];
+        }
+    }
+}
