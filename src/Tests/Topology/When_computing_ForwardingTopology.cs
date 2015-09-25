a051c23676a94244fa70cf2b2718006dd4ec7e50
Sean Feldman <feldman.sean@gmail.com>
2015-09-25 00:10:36 -0600

    Adding more verifications about ForwardingTopology behavior

--- src/Tests/Topology/When_computing_ForwardingTopology.cs
+++ src/Tests/Topology/When_computing_ForwardingTopology.cs
@@ -1,5 +1,6 @@
 namespace NServiceBus.AzureServiceBus.Tests
 {
+    using System.Collections.Generic;
     using System.Linq;
     using Settings;
     using NUnit.Framework;
@@ -53,6 +54,59 @@ namespace NServiceBus.AzureServiceBus.Tests
             Assert.AreEqual(1, definition.Entities.Count(ei => ei.Path == "sales" && ei.Type == EntityType.Queue && ei.Namespace.ConnectionString == connectionstring));
         }
 
-       
+        [Test]
+        public void Determines_there_should_be_a_topic_bundle_created()
+        {
+            var container = new FuncBuilder();
+
+            var settings = new SettingsHolder();
+            container.Register(typeof(SettingsHolder), () => settings);
+            var extensions = new TransportExtensions<AzureServiceBusTransport>(settings);
+
+            settings.SetDefault<EndpointName>(new EndpointName("sales"));
+            const string connectionstring = "Endpoint=sb://namespace.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=somesecretkey";
+            extensions.Topology().Addressing().NamespacePartitioning().AddNamespace(connectionstring);
+
+            var topology = new ForwardingTopology(settings, container);
+
+            topology.InitializeSettings();
+            topology.InitializeContainer();
+            var definition = topology.Determine(Purpose.Creating);
+
+            var result = definition.Entities.Where(ei => ei.Type == EntityType.Topic && ei.Namespace.ConnectionString == connectionstring && ei.Path.StartsWith("bundle-"));
+
+            Assert.That(result.Count(), Is.EqualTo(2));
+            Assert.That(result, Has.Exactly(1).Matches<EntityInfo>(x => x.Path == "bundle-1"));
+            Assert.That(result, Has.Exactly(1).Matches<EntityInfo>(x => x.Path == "bundle-2"));
+        }
+
+        [Test]
+        public void Creates_subscription_on_each_topic_in_bundle()
+        {
+            var container = new FuncBuilder();
+
+            var settings = new SettingsHolder();
+            container.Register(typeof(SettingsHolder), () => settings);
+            var extensions = new TransportExtensions<AzureServiceBusTransport>(settings);
+
+            settings.SetDefault<EndpointName>(new EndpointName("sales"));
+            const string connectionstring = "Endpoint=sb://namespace.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=somesecretkey";
+            extensions.Topology().Addressing().NamespacePartitioning().AddNamespace(connectionstring);
+
+            var topology = new ForwardingTopology(settings, container);
+
+            topology.InitializeSettings();
+            topology.InitializeContainer();
+            topology.Determine(Purpose.Creating);
+
+            var subscriptionInfos = topology.Subscribe(typeof(SomeEvent));
+
+            Assert.That(subscriptionInfos.Count(), Is.EqualTo(2));
+            // TODO: need to verify that subscription is done on each topic
+        }
+
+        class SomeEvent
+        {
+        }
     }
 }
