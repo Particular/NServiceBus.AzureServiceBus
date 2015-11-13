f6be6077bd9faf1bb78d15b38b4296a03b59d46e
Sean Feldman <feldman.sean@gmail.com>
2015-11-13 00:18:56 -0700

    verification for retries  - BrokeredMessage clones to be used on retries

---
+++ src/Tests/Sending/When_routing_outgoingmessages_to_endpoints_with_retries.cs
@@ -0,0 +1,66 @@
+namespace NServiceBus.AzureServiceBus.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using FakeItEasy;
+    using Microsoft.ServiceBus.Messaging;
+    using NServiceBus.Azure.WindowsAzureServiceBus.Tests;
+    using NServiceBus.DeliveryConstraints;
+    using NServiceBus.Routing;
+    using NServiceBus.Settings;
+    using NServiceBus.Transports;
+    using NUnit.Framework;
+
+    [TestFixture]
+    [Category("AzureServiceBus")]
+    public class When_routing_outgoingmessages_to_endpoints_with_retries
+    {
+        [Test]
+        public async Task Should_use_cloned_original_brokered_messages_on_retries()
+        {
+            // default settings
+            var settings = new DefaultConfigurationValues().Apply(new SettingsHolder());
+            var messageSenderSettings = new AzureServiceBusMessageSenderSettings(settings).BackOffTimeOnThrottle(TimeSpan.Zero);
+            messageSenderSettings.RetryAttemptsOnThrottle(1);
+
+            var topology = A.Fake<ITopology>();
+            var clientLifecycleManager = A.Fake<IManageMessageSenderLifeCycle>();
+            var messageSender = A.Fake<IMessageSender>();
+
+            var usedMessages = new List<BrokeredMessage>();
+
+            A.CallTo(() => topology.DetermineSendDestination(A<string>._))
+                .Returns(new TopologySection
+                {
+                    Entities = new[]
+                    {
+                        new EntityInfo
+                        {
+                            Path = "MyQueue",
+                            Namespace = new NamespaceInfo(AzureServiceBusConnectionString.Value, NamespaceMode.Active)
+                        }
+                    }
+                });
+
+            A.CallTo(() => clientLifecycleManager.Get(A<string>.Ignored, A<string>.Ignored, A<string>.Ignored))
+                .Returns(messageSender);
+
+            A.CallTo(() => messageSender.SendBatchAsync(A<IEnumerable<BrokeredMessage>>._))
+                .Invokes((IEnumerable<BrokeredMessage> received) => usedMessages.Add(received.First()))
+                .Throws(new ServerBusyException("busy"));
+
+            var router = new DefaultOutgoingMessageRouter(
+                topology,
+                new DefaultOutgoingMessagesToBrokeredMessagesConverter(settings), 
+                clientLifecycleManager, settings, new FuncBuilder(new FuncContainer()));
+
+            var outgoingMessage = new OutgoingMessage("SomeId", new Dictionary<string, string>(), new byte[] {});
+            var dispatchOptions = new DispatchOptions(new UnicastAddressTag("MyQueue"), DispatchConsistency.Default, new List<DeliveryConstraint>());
+
+            Assert.That(async () => await router.RouteBatchAsync(new[] { outgoingMessage }, new RoutingOptions {DispatchOptions = dispatchOptions}), Throws.Exception);
+            Assert.AreNotSame(usedMessages[0], usedMessages[1], "retried message should be a clone and not the original BrokeredMessage");
+        }
+    }
+}
