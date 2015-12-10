74955bcdbe125627ca07aa07953fa4ef87b10a98
Sean Feldman <feldman.sean@gmail.com>
2015-12-10 01:36:01 -0700

    Removing TearDown approach -- too slow

--- src/AcceptanceTests/SetupAcceptanceTests.cs
+++ src/AcceptanceTests/SetupAcceptanceTests.cs
@@ -22,56 +22,5 @@ public class SetupAcceptanceTests
         // setting topology for all ATTs
         // setting.Set<TransportDefinition>(topology_to_use)
     }
-
-    private string ConnectionString { get; } = Environment.GetEnvironmentVariable("AzureServiceBusTransport.ConnectionString");
-
-    [TestFixtureTearDown]
-    public void TearDown()
-    {
-        var stopwatch = new Stopwatch();
-        stopwatch.Start();
-
-        var namespaceManager = NamespaceManager.CreateFromConnectionString(ConnectionString);
-
-        var topics = namespaceManager.GetTopics();
-        Parallel.ForEach(topics, topic =>
-        {
-            var subscriptions = namespaceManager.GetSubscriptions(topic.Path);
-
-            Task.WaitAll(subscriptions.Select(x => ClearSubscription(topic.Path, x.Name)).ToArray());
-        });
-
-        Task.WaitAll(namespaceManager.GetQueues().Select(q => ClearQueue(q.Path)).ToArray());
-
-        stopwatch.Stop();
-        Console.WriteLine("Time to cleanup: {0} seconds", TimeSpan.FromMilliseconds(stopwatch.ElapsedMilliseconds).TotalSeconds);
-    }
-
-    const int BATCH_SIZE_FOR_CLEARING = 1000;
-    const int SECONDS_TO_WAIT_FOR_BATCH = 10;
-
-    async Task ClearQueue(string queuePath)
-    {
-        var client = QueueClient.CreateFromConnectionString(ConnectionString, queuePath, ReceiveMode.ReceiveAndDelete);
-        IEnumerable<BrokeredMessage> messages;
-        do
-        {
-            messages = await client.ReceiveBatchAsync(BATCH_SIZE_FOR_CLEARING, TimeSpan.FromSeconds(SECONDS_TO_WAIT_FOR_BATCH));
-        } while (messages.Any());
-
-        Console.WriteLine("Cleared '{0}' queue", queuePath);
-    }
-
-    async Task ClearSubscription(string topicPath, string name)
-    {
-        var client = SubscriptionClient.CreateFromConnectionString(ConnectionString, topicPath, name, ReceiveMode.ReceiveAndDelete);
-        IEnumerable<BrokeredMessage> messages;
-        do
-        {
-            messages = await client.ReceiveBatchAsync(BATCH_SIZE_FOR_CLEARING, TimeSpan.FromSeconds(SECONDS_TO_WAIT_FOR_BATCH));
-        } while (messages.Any());
-
-        Console.WriteLine("Cleared '{0}->{1}' subscription", topicPath, name);
-    }
 }
 
