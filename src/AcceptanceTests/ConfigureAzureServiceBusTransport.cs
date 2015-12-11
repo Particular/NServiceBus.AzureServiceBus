d4f4706c37f37dc2da942c643ef4b6f5f6aaaa2e
Sean Feldman <feldman.sean@gmail.com>
2015-12-10 17:37:12 -0700

    change needed in core (https://github.com/Particular/NServiceBus/pull/3203)

---
+++ src/AcceptanceTests/ConfigureAzureServiceBusTransport.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Configuration.AdvanceExtensibility;
+using NServiceBus.Transports;
+
+public class ConfigureAzureServiceBusTransport
+{
+    BusConfiguration busConfiguration;
+
+    public Task Configure(BusConfiguration configuration)
+    {
+        busConfiguration = configuration;
+        return Task.FromResult(0);
+    }
+
+    public Task Cleanup()
+    {
+        var bindings = busConfiguration.GetSettings().Get<QueueBindings>();
+       
+        return Task.FromResult(0);
+    }
+}
