29d634a0f23872350840e68b8dc722b1c40e526f
Yves Goeleven <yves@goeleven.com>
2015-12-15 20:33:19 +0100

    removed unused using

--- src/Transport/Config/DefaultConfigurationValues.cs
+++ src/Transport/Config/DefaultConfigurationValues.cs
@@ -3,7 +3,6 @@
     using System;
     using System.Collections.Generic;
     using Microsoft.ServiceBus.Messaging;
-    using NServiceBus.Features;
     using NServiceBus.Settings;
 
     public class DefaultConfigurationValues
