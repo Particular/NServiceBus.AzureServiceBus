131c8ad2d5351ec642e541c9fdcf6dd3dcccd93e
Yves Goeleven <yves@goeleven.com>
2015-12-18 14:22:14 +0100

    changed the scope of dispatchoptions

--- src/Transport/Sending/RoutingOptions.cs
+++ src/Transport/Sending/RoutingOptions.cs
@@ -1,13 +1,10 @@
 namespace NServiceBus.AzureServiceBus
 {
-    using NServiceBus.Transports;
-
     public class RoutingOptions
     {
         public bool SendVia { get; set; }
         public string ViaEntityPath { get; set; }
         public string ViaConnectionString { get; set; }
-        public DispatchOptions DispatchOptions { get; set; }
         public string ViaPartitionKey { get; set; }
     }
 }
