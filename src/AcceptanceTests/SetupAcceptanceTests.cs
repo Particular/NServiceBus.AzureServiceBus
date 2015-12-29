5f7e22c08b236a16f10cf5b2483350a6ec438b02
Yves Goeleven <yves@goeleven.com>
2015-12-29 11:46:31 +0100

    via routing for a subscription can be done through it's parent topic

--- src/AcceptanceTests/SetupAcceptanceTests.cs
+++ src/AcceptanceTests/SetupAcceptanceTests.cs
@@ -1,3 +1,4 @@
+using NServiceBus.AzureServiceBus.AcceptanceTests;
 using NUnit.Framework;
 
 /// <summary>
@@ -9,12 +10,6 @@ public class SetupAcceptanceTests
     [SetUp]
     public void SetUp()
     {
-        // case 1:
-        // ConfigureTopology = (trans) => (trans as ASB).Topology = Activator.Create(env["topology"]) // for "metadata" about the transport
-        // case 2:
-        // setting topology for all ATTs
-        // setting.Set<TransportDefinition>(topology_to_use)
-
         //fix time
         //var timeSync = new TimeSynchronization();
         //timeSync.Sync();
