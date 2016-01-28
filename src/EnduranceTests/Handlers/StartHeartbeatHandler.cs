d82335dbf2dcf94396b7de0d4424975beb613027
Bob Langley <Bob Langley>
2016-01-28 14:30:31 -0800

    Initial commit. Still needs some measurement / detection mechanism

---
+++ src/EnduranceTests/Handlers/StartHeartbeatHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace NServiceBus.AzureServiceBus.EnduranceTests.Handlers
+{
+    using System.Threading.Tasks;
+    using NServiceBus.AzureServiceBus.EnduranceTests.Commands;
+    using NServiceBus.Logging;
+
+    public class StartHeartbeatHandler : IHandleMessages<StartHeartbeat>
+    {
+        private static readonly ILog log = LogManager.GetLogger<StartHeartbeatHandler>();
+        public Task Handle(StartHeartbeat message, IMessageHandlerContext context)
+        {
+            log.InfoFormat("StartHeartbeatHandler starting Heartbeat with interval of {0}", message.Wait);
+            TestSettings.TestEnabled = true;
+            return context.Send<Heartbeat>(cmd => { cmd.Wait = message.Wait; }, TestSettings.SendOptions);
+        }
+    }
+}
