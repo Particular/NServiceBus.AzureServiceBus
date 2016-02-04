b647b2c45b6d42aafb04bc64e3ea66406763e394
Bob Langley <Bob Langley>
2016-02-04 08:02:16 -0800

    Slack / Blob logging of unhandled exceptions is working. AggregateException handling needs to be fixed before PR

---
+++ src/EnduranceTests/Targets/AzureBlobErrorOnlyTarget.cs
@@ -0,0 +1,54 @@
+﻿
+namespace NServiceBus.AzureServiceBus.EnduranceTests.Targets
+{
+    using System.Diagnostics;
+    using Microsoft.WindowsAzure.Storage;
+    using Microsoft.WindowsAzure.Storage.Blob;
+    using NLog;
+    using NLog.Targets;
+    using NServiceBus.AzureServiceBus.EnduranceTests.TestUtils;
+    using LogLevel = NLog.LogLevel;
+
+    public class AzureBlobErrorOnlyTarget : TargetWithLayout
+    {
+        private CloudBlobClient _client;
+
+        public CloudStorageAccount StorageAccount { get; set; }
+
+        protected override void InitializeTarget()
+        {
+            base.InitializeTarget();
+
+            _client = StorageAccount.CreateCloudBlobClient();
+        }
+
+        protected override void Write(LogEventInfo logEvent)
+        {
+            if (logEvent.Level < LogLevel.Error) return;
+
+            if (_client == null) return;
+
+            var container = _client.GetContainerReference(BlobNameBuilder.BuildContainerName(logEvent));
+            container.CreateIfNotExists();
+
+            var blobName = BlobNameBuilder.Build(logEvent);
+
+            var blob = container.GetBlockBlobReference(blobName);
+
+            var index = 0;
+            while (blob.Exists())
+            {
+                index++;
+                blob = container.GetBlockBlobReference(blobName + "_" + index);
+            }
+
+            blob.Properties.ContentType = "text/plain";
+
+            var logMessage = Layout.Render(logEvent);
+
+            blob.UploadText(logMessage);
+
+            Debug.WriteLine("Writing to blob storage: " + blob.Uri);
+        }
+    }
+}
