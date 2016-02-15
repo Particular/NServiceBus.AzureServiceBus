e1f05d03be3e90deeb45e2a3cd5a2bb3a33079e2
Yves Goeleven <yves@goeleven.com>
2016-02-15 14:54:20 +0100

    upgraded to latest

--- src/Transport/Config/AzureServiceBusTransport.cs
+++ src/Transport/Config/AzureServiceBusTransport.cs
@@ -1,21 +1,38 @@
 ﻿namespace NServiceBus
 {
+    using System.Collections.Generic;
+    using System.Linq;
     using NServiceBus.AzureServiceBus;
     using NServiceBus.Settings;
     using NServiceBus.Transports;
 
     public class AzureServiceBusTransport : TransportDefinition
     {
-        protected override TransportInfrastructure Initialize(SettingsHolder settings)
+        protected override TransportInfrastructure Initialize(SettingsHolder settings, string connectionString)
         {
             settings.SetDefault("Transactions.DoNotWrapHandlersExecutionInATransactionScope", true);
             settings.SetDefault("Transactions.SuppressDistributedTransactions", true);
             settings.SetDefault<ITopology>(new StandardTopology());
-
+            
             var topology = settings.Get<ITopology>();
             topology.Initialize(settings);
 
+            RegisterConnectionStringAsNamespace(connectionString, settings);
+
             return new AzureServiceBusTransportInfrastructure(topology, settings);
         }
+
+        private void RegisterConnectionStringAsNamespace(string connectionstring, ReadOnlySettings settings)
+        {
+            var namespaces = settings.Get<List<string>>(WellKnownConfigurationKeys.Topology.Addressing.Partitioning.Namespaces);
+            if (namespaces.All(n => n != connectionstring))
+            {
+                namespaces.Add(connectionstring);
+            }
+        }
+
+        public override bool RequiresConnectionString { get; } = true;
+
+        public override string ExampleConnectionStringForErrorMessage { get; } = "Endpoint=sb://[namespace].servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=[secret_key]";
     }
 }
--- src/Transport/Config/AzureServiceBusTransportInfrastructure.cs
+++ src/Transport/Config/AzureServiceBusTransportInfrastructure.cs
@@ -2,7 +2,6 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Linq;
     using System.Threading.Tasks;
     using NServiceBus.AzureServiceBus;
     using NServiceBus.DelayedDelivery;
@@ -14,15 +13,13 @@
     public class AzureServiceBusTransportInfrastructure : TransportInfrastructure
     {
         private readonly ITopology topology;
-        private readonly SettingsHolder settings;
 
         public AzureServiceBusTransportInfrastructure(ITopology topology, SettingsHolder settings)
         {
             this.topology = topology;
-            this.settings = settings;
         }
 
-        public override EndpointInstance BindToLocalEndpoint(EndpointInstance instance, ReadOnlySettings settings)
+        public override EndpointInstance BindToLocalEndpoint(EndpointInstance instance)
         {
             return instance;
         }
@@ -32,18 +29,16 @@
             return logicalAddress.ToString();
         }
 
-        public override TransportReceiveInfrastructure ConfigureReceiveInfrastructure(string connectionString)
+        public override TransportReceiveInfrastructure ConfigureReceiveInfrastructure()
         {
-            EnsureConnectionStringIsRegisteredAsNamespace(connectionString, settings);
             return new TransportReceiveInfrastructure(
                 topology.GetMessagePumpFactory(),
                 topology.GetQueueCreatorFactory(),
                 () => Task.FromResult(StartupCheckResult.Success));
         }
 
-        public override TransportSendInfrastructure ConfigureSendInfrastructure(string connectionString)
+        public override TransportSendInfrastructure ConfigureSendInfrastructure()
         {
-            EnsureConnectionStringIsRegisteredAsNamespace(connectionString, settings);
             return new TransportSendInfrastructure(
                 topology.GetDispatcherFactory(),
                 () => Task.FromResult(StartupCheckResult.Success));
@@ -53,8 +48,6 @@
         {
             return new TransportSubscriptionInfrastructure(topology.GetSubscriptionManagerFactory());
         }
-
-        public override string ExampleConnectionStringForErrorMessage { get; } = "Endpoint=sb://[namespace].servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=[secret_key]";
 
         public override IEnumerable<Type> DeliveryConstraints
         {
@@ -79,14 +72,6 @@
                 return topology.GetOutboundRoutingPolicy();
             }
         }
-
-        private void EnsureConnectionStringIsRegisteredAsNamespace(string connectionstring, ReadOnlySettings settings)
-        {
-            var namespaces = settings.Get<List<string>>(WellKnownConfigurationKeys.Topology.Addressing.Partitioning.Namespaces);
-            if (namespaces.All(n => n != connectionstring))
-            {
-                namespaces.Add(connectionstring);
-            }
-        }
+        
     }
 }
