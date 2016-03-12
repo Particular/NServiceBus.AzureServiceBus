a088cc894c1de4cbfe4a6883c5f5f86593acc870
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2016-03-12 20:30:26 +0100

    Moved TypeExtensions to Utils folder

--- src/Transport/Topology/PublishersConfiguration.cs
+++ src/Transport/Topology/PublishersConfiguration.cs
@@ -52,21 +52,4 @@
             return _publishers.ContainsKey(type);
         }
     }
-
-    internal static class TypeExtensions
-    {
-        public static IEnumerable<Type> GetParentTypes(this Type target)
-        {
-            foreach (var i in target.GetInterfaces())
-                yield return i;
-
-            var currentBaseType = target.BaseType;
-            var objectType = typeof(object);
-            while (currentBaseType != null && currentBaseType != objectType)
-            {
-                yield return currentBaseType;
-                currentBaseType = currentBaseType.BaseType;
-            }
-        }
-    }
 }
