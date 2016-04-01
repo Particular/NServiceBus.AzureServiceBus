c9e198bd150f2ba89ca705a75bcc4c2b7afa15d3
Sean Feldman <feldman.sean@gmail.com>
2016-04-01 13:57:30 -0600

    Upgrade to core without IWantToRunWhenBusStartsAndStops
    * core upgrade to unstable1782
    
    * remove IWantToRunWhenBusStartsAndStops for custom acceptance test

--- src/AcceptanceTests/Routing/When_subscribing_to_a_derived_polymorphic_event_with_forwarding_topology.cs
+++ src/AcceptanceTests/Routing/When_subscribing_to_a_derived_polymorphic_event_with_forwarding_topology.cs
@@ -16,25 +16,25 @@
         public async Task Should_handle_each_event_once_only()
         {
             var context = await Scenario.Define<Context>()
-                    // Publish event only when it was signaled that events went out
-                    .WithEndpoint<Publisher>(b => b.When(c => c.ReceiverSubscribedToEvents, async bus =>
-                    {
-                        await bus.Publish<DerivedEvent>();
+                // Publish event only when it was signaled that events went out
+                .WithEndpoint<Publisher>(b => b.When(c => c.ReceiverSubscribedToEvents, async bus =>
+                {
+                    await bus.Publish<DerivedEvent>();
 
-                        // Delay signal command to stop by 2 seconds to ensure there was enough time for duplicate events
-                        var sendOptions = new SendOptions();
-                        sendOptions.DelayDeliveryWith(TimeSpan.FromSeconds(2));
-                        await bus.Send(new EventWasRaisedSoStopProcessing(), sendOptions);
-                    }))
-                    .WithEndpoint<Subscriber>(b => b.When(async (session, c) =>
-                    {
-                        await session.Subscribe<BaseEvent>();
-                        await session.Subscribe<DerivedEvent>();
+                    // Delay signal command to stop by 2 seconds to ensure there was enough time for duplicate events
+                    var sendOptions = new SendOptions();
+                    sendOptions.DelayDeliveryWith(TimeSpan.FromSeconds(2));
+                    await bus.Send(new EventWasRaisedSoStopProcessing(), sendOptions);
+                }))
+                .WithEndpoint<Subscriber>(b => b.When(async (session, c) =>
+                {
+                    await session.Subscribe<BaseEvent>();
+                    await session.Subscribe<DerivedEvent>();
 
-                        c.ReceiverSubscribedToEvents = true;
-                    }))
-                    .Done(ctx => ctx.StopCommandWasReceived && ctx.IsForwardingTopology || !ctx.IsForwardingTopology)
-                    .Run();
+                    c.ReceiverSubscribedToEvents = true;
+                }))
+                .Done(ctx => ctx.StopCommandWasReceived && ctx.IsForwardingTopology || !ctx.IsForwardingTopology)
+                .Run();
 
             if (!context.IsForwardingTopology)
             {
@@ -60,28 +60,55 @@
 
             public Publisher()
             {
-                EndpointSetup<DefaultPublisher>()
+                EndpointSetup<DefaultPublisher>(endpointConfiguration => endpointConfiguration.EnableFeature<DetermineWhatTopologyIsUsed>())
                     .AddMapping<EventWasRaisedSoStopProcessing>(typeof(Subscriber));
             }
 
-            class DetermineWhatTopologyIsUsed : IWantToRunWhenBusStartsAndStops
+            class DetermineWhatTopologyIsUsed : Feature
             {
-                public Context Context { get; set; }
-
-                public ReadOnlySettings Settings { get; set; }
-
-                public Task Start(IMessageSession session)
+                //                public Context Context { get; set; }
+                //
+                //                public ReadOnlySettings Settings { get; set; }
+                //
+                //                public Task Start(IMessageSession session)
+                //                {
+                //                    Context.IsForwardingTopology = Settings.Get<ITopology>() is ForwardingTopology;
+                //                    return Task.FromResult(0);
+                //                }
+                //
+                //                public Task Stop(IMessageSession session)
+                //                {
+                //                    return Task.FromResult(0);
+                protected override void Setup(FeatureConfigurationContext context)
                 {
-                    Context.IsForwardingTopology = Settings.Get<ITopology>() is ForwardingTopology;
-                    return Task.FromResult(0);
+                    context.RegisterStartupTask(builder => new TaskToDetermineCurrentTopology(builder.Build<Context>(), builder.Build<ReadOnlySettings>()));
+                }
+            }
+
+            class TaskToDetermineCurrentTopology : FeatureStartupTask
+            {
+                Context context;
+                ReadOnlySettings settings;
+
+                public TaskToDetermineCurrentTopology(Context context, ReadOnlySettings settings)
+                {
+                    this.context = context;
+                    this.settings = settings;
                 }
 
-                public Task Stop(IMessageSession session)
+                protected override Task OnStart(IMessageSession session)
                 {
-                    return Task.FromResult(0);
+                    context.IsForwardingTopology = settings.Get<ITopology>() is ForwardingTopology;
+                    return TaskEx.Completed;
+                }
+
+                protected override Task OnStop(IMessageSession session)
+                {
+                    return TaskEx.Completed;
                 }
             }
         }
+
 
         public class Subscriber : EndpointConfigurationBuilder
         {
@@ -133,7 +160,7 @@
 
         public class EventWasRaisedSoStopProcessing : ICommand
         {
-             
+
         }
     }
 }
