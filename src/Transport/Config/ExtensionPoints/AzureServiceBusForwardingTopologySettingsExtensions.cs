a986a0e87cad497424ad6233cac3340eafedbc6b
Yves Goeleven <yves@goeleven.com>
2016-04-18 10:20:35 +0200

    forwarding topo config settings had no extension method

---
+++ src/Transport/Config/ExtensionPoints/AzureServiceBusForwardingTopologySettingsExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace NServiceBus
+{
+    using AzureServiceBus;
+    using Configuration.AdvanceExtensibility;
+
+    public static class AzureServiceBusForwardingTopologySettingsExtensions
+    {
+        public static AzureServiceBusTopologySettings<ForwardingTopology> NumberOfEntitiesInBundle(this AzureServiceBusTopologySettings<ForwardingTopology> topologySettings, int number)
+        {
+            var settings = topologySettings.GetSettings();
+            settings.Set(WellKnownConfigurationKeys.Topology.Bundling.NumberOfEntitiesInBundle, number);
+            return topologySettings;
+        }
+
+        public static AzureServiceBusTopologySettings<ForwardingTopology> BundlePrefix(this AzureServiceBusTopologySettings<ForwardingTopology> topologySettings, string prefix)
+        {
+            var settings = topologySettings.GetSettings();
+            settings.Set(WellKnownConfigurationKeys.Topology.Bundling.BundlePrefix, prefix);
+            return topologySettings;
+        }
+    }
+}
