5c9030cfb8510834341b834e8f914f9a7cfbca97
Yves Goeleven <yves@goeleven.com>
2016-05-23 13:58:24 +0200

    lock token completion only in peek lock mode

--- src/Transport/Receiving/MessageReceiverNotifier.cs
+++ src/Transport/Receiving/MessageReceiverNotifier.cs
@@ -188,7 +188,10 @@ namespace NServiceBus.AzureServiceBus
                 else
                 {
                     await InvokeCompletionCallbacksAsync(context).ConfigureAwait(false);
-                    locksTokensToComplete.Push(message.LockToken);
+                    if (context.ReceiveMode == ReceiveMode.PeekLock)
+                    {
+                        locksTokensToComplete.Push(message.LockToken);
+                    }
                 }
             }
             catch (Exception exception)
