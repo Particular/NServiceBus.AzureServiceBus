9b17d56171a6645cf500c3b50e4c7d4817140b66
Sean Feldman <feldman.sean@gmail.com>
2016-06-06 15:48:24 -0600

    ValidationResult to be used by IValidationStrategy instead of boolean

---
+++ src/Transport/Addressing/Validation/ValidationResult.cs
@@ -0,0 +1,20 @@
+﻿namespace NServiceBus.AzureServiceBus.Addressing
+{
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Linq;
+
+    public class ValidationResult
+    {
+        List<string> validationErrors = new List<string>();
+
+        public void AddError(string error)
+        {
+            validationErrors.Add(error);
+        }
+
+        public bool IsValid => !validationErrors.Any();
+
+        public ReadOnlyCollection<string> Errors => validationErrors.AsReadOnly();
+    }
+}
