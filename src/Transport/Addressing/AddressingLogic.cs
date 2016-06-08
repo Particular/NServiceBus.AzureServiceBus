a675e7784d71baae187e958c33cf4d169128a2cf
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2016-06-08 14:29:07 +0200

    added capabilities to extract entity name without suffix into addressing logic

--- src/Transport/Addressing/AddressingLogic.cs
+++ src/Transport/Addressing/AddressingLogic.cs
@@ -1,5 +1,7 @@
 ﻿namespace NServiceBus.AzureServiceBus.Addressing
 {
+    using Topology.MetaModel;
+
     class AddressingLogic
     {
         readonly IValidationStrategy validationStrategy;
@@ -15,6 +17,9 @@
 
         public string Apply(string entityname, EntityType entityType)
         {
+            var address = new EntityAddress(entityname);
+            entityname = address.Name;
+
             var path = composition.GetEntityPath(entityname, entityType);
             var validationResult = validationStrategy.IsValid(path, entityType);
             if (!validationResult.IsValid)
