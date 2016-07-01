c11b12c3815b4fd359f1ac177764ffdd605c040b
Yves Goeleven <yves@goeleven.com>
2016-07-01 18:57:23 +0200

    damn inspections

--- src/Transport/Topology/MetaModel/PassThroughNamespaceNameToConnectionStringMapper.cs
+++ src/Transport/Topology/MetaModel/PassThroughNamespaceNameToConnectionStringMapper.cs
@@ -1,17 +1,7 @@
 namespace NServiceBus.AzureServiceBus.Topology.MetaModel
 {
-    using System.Linq;
-    using Settings;
-
     class PassThroughNamespaceNameToConnectionStringMapper : ICanMapNamespaceNameToConnectionString
     {
-        ReadOnlySettings settings;
-
-        public PassThroughNamespaceNameToConnectionStringMapper(ReadOnlySettings settings)
-        {
-            this.settings = settings;
-        }
-
         public EntityAddress Map(EntityAddress value)
         {
             return new EntityAddress(value);
