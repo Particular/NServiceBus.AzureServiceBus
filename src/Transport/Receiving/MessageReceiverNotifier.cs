94b983244607f9de45979d02505e3f3d13e60109
Yves Goeleven <yves@goeleven.com>
2016-07-29 11:47:49 +0200

    fixed circuit breaker test

--- src/Transport/Receiving/MessageReceiverNotifier.cs
+++ src/Transport/Receiving/MessageReceiverNotifier.cs
@@ -3,7 +3,6 @@ namespace NServiceBus.AzureServiceBus
     using System;
     using System.Collections.Concurrent;
     using System.Collections.Generic;
-    using System.IO;
     using System.Linq;
     using System.Threading;
     using System.Threading.Tasks;
