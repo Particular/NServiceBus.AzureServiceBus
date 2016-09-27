945d7c39d0c77976d3d690824af253a54747be63
Sean Feldman <feldman.sean@gmail.com>
2016-09-27 08:01:21 -0600

    Decide on the prefetch count when transport is Initialized (user settings are applied, but settings holder is not readonly yet)

--- src/Transport/Connectivity/MessageReceiverCreator.cs
+++ src/Transport/Connectivity/MessageReceiverCreator.cs
@@ -1,14 +1,12 @@
 namespace NServiceBus.Transport.AzureServiceBus
 {
     using System.Threading.Tasks;
-    using Logging;
     using Microsoft.ServiceBus;
     using Microsoft.ServiceBus.Messaging;
     using Settings;
 
     class MessageReceiverCreator : ICreateMessageReceivers
     {
-        static ILog logger = LogManager.GetLogger(typeof(MessageReceiverCreator));
         IManageMessagingFactoryLifeCycle factories;
         ReadOnlySettings settings;
 
@@ -18,26 +16,15 @@ namespace NServiceBus.Transport.AzureServiceBus
             this.settings = settings;
         }
 
-
         public async Task<IMessageReceiver> Create(string entityPath, string namespaceAlias)
         {
             var factory = factories.Get(namespaceAlias);
             var receiveMode = settings.Get<ReceiveMode>(WellKnownConfigurationKeys.Connectivity.MessageReceivers.ReceiveMode);
 
-            var prefetchCount = settings.GetOrDefault<int>(WellKnownConfigurationKeys.Connectivity.MessageReceivers.PrefetchCount);
-            var userHasNotProvidedPrefetchCount = !settings.HasExplicitValue(WellKnownConfigurationKeys.Connectivity.MessageReceivers.PrefetchCount);
-            var transportTransactionMode = settings.HasExplicitValue<TransportTransactionMode>() ? settings.Get<TransportTransactionMode>() : settings.SupportedTransactionMode();
-
-            // do not allow prefetch if user hasn't explicitly instructed to use a prefetch count of a certain size when ReceiveAndDelete mode is used (TransTxMode.None)
-            if (userHasNotProvidedPrefetchCount && (transportTransactionMode == TransportTransactionMode.None || receiveMode == ReceiveMode.ReceiveAndDelete))
-            {
-                prefetchCount = 0;
-                logger.Warn("Mesasge receiver PrefetchCount was reduced to zero because to avoid message loss with ReceiveAndDelete receive mode. To enforce prefetch, use configuration API to set the value explicitly.");
-            }
-
             var receiver = await factory.CreateMessageReceiver(entityPath, receiveMode).ConfigureAwait(false);
-            receiver.PrefetchCount = prefetchCount;
-            
+
+            receiver.PrefetchCount = settings.Get<int>(WellKnownConfigurationKeys.Connectivity.MessageReceivers.PrefetchCount);
+
             if (settings.HasExplicitValue(WellKnownConfigurationKeys.Connectivity.MessageReceivers.RetryPolicy))
             {
                 receiver.RetryPolicy = settings.Get<RetryPolicy>(WellKnownConfigurationKeys.Connectivity.MessageReceivers.RetryPolicy);
