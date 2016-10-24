739b221bebd6d9f987a43d5e9b18a85c20ca4086
Sean Feldman <feldman.sean@gmail.com>
2016-10-24 16:50:25 -0600

    obsoleting INotifyIncomingMessages contract

--- src/Transport/Receiving/INotifyIncomingMessages.cs
+++ src/Transport/Receiving/INotifyIncomingMessages.cs
@@ -4,6 +4,7 @@ namespace NServiceBus.Transport.AzureServiceBus
     using System.Threading.Tasks;
     using Transport;
 
+    [Obsolete("Internal contract that shouldn't be exposed. Will be treated as an error from version 8.0.0. Will be removed in version 9.0.0.", false)]
     public interface INotifyIncomingMessages
     {
         bool IsRunning { get; }
