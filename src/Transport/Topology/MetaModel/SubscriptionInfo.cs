a23cb20486a7a5d8313ddf67a53bae8d395ec42a
Sean Feldman <feldman.sean@gmail.com>
2016-10-24 22:46:27 -0600

    Never used IClientSideSubscriptionFilter

--- src/Transport/Topology/MetaModel/SubscriptionInfo.cs
+++ src/Transport/Topology/MetaModel/SubscriptionInfo.cs
@@ -4,6 +4,7 @@
     {
         public IBrokerSideSubscriptionFilter BrokerSideFilter { get; set; }
 
+        [ObsoleteEx(Message = "Internal contract that shouldn't be exposed.", TreatAsErrorFromVersion = "8.0", RemoveInVersion = "9.0")]
         public IClientSideSubscriptionFilter ClientSideFilter { get; set; }
 
         public SubscriptionMetadata Metadata { get; set; }
