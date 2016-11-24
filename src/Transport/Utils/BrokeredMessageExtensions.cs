c11fa1827e492a81aad016a0eabf139ccc0c7540
Daniel Marbach <daniel.marbach@openplace.net>
2016-11-24 18:27:30 +0100

    Fix spelling of occured to occurred (#427)

--- src/Transport/Utils/BrokeredMessageExtensions.cs
+++ src/Transport/Utils/BrokeredMessageExtensions.cs
@@ -19,28 +19,28 @@ namespace NServiceBus.Transport.AzureServiceBus
             catch (MessageLockLostException ex)
             {
                 // It's too late to compensate the loss of a message lock. We should just ignore it so that it does not break the receive loop.
-                Log.Warn($"A message lock lost exception occured while trying to complete a message, you may consider to increase the lock duration or reduce the batch size, the exception was {ex.Message}", ex);
+                Log.Warn($"A message lock lost exception occurred while trying to complete a message, you may consider to increase the lock duration or reduce the batch size, the exception was {ex.Message}", ex);
             }
             catch (MessagingException ex)
             {
                 // There is nothing we can do as the connection may have been lost, or the underlying queue may have been removed.
                 // If Complete() fails with this exception, the only recourse is to receive another message.
-                Log.Warn($"A messaging exception occured while trying to complete a message, this might imply that the connection was lost or the underlying queue got removed, the exception was {ex.Message}", ex);
+                Log.Warn($"A messaging exception occurred while trying to complete a message, this might imply that the connection was lost or the underlying queue got removed, the exception was {ex.Message}", ex);
             }
             catch (ObjectDisposedException ex)
             {
                 // There is nothing we can do as the object has already been disposed elsewhere
-                Log.Warn($"An object disposed exception occured while trying to complete a message, this might imply that the connection was lost or the underlying queue got removed, the exception was {ex.Message}", ex);
+                Log.Warn($"An object disposed exception occurred while trying to complete a message, this might imply that the connection was lost or the underlying queue got removed, the exception was {ex.Message}", ex);
             }
             catch (TransactionException ex)
             {
                 // ASB Sdk beat us to it
-                Log.Warn($"A transaction exception occured while trying to complete a message, this probably means that the Azure ServiceBus SDK has rolled back the transaction already, the exception was {ex.Message}", ex);
+                Log.Warn($"A transaction exception occurred while trying to complete a message, this probably means that the Azure ServiceBus SDK has rolled back the transaction already, the exception was {ex.Message}", ex);
             }
             catch (TimeoutException ex)
             {
                 // took to long
-                Log.Warn($"A timeout exception occured while trying to complete a message, the exception was {ex.Message}", ex);
+                Log.Warn($"A timeout exception occurred while trying to complete a message, the exception was {ex.Message}", ex);
             }
             return false;
         }
@@ -59,28 +59,28 @@ namespace NServiceBus.Transport.AzureServiceBus
             catch (MessageLockLostException ex)
             {
                 // It's too late to compensate the loss of a message lock. We should just ignore it so that it does not break the receive loop.
-                Log.Warn($"A message lock lost exception occured while trying to abandon a message, you may consider to increase the lock duration or reduce the batch size, the exception was {ex.Message}", ex);
+                Log.Warn($"A message lock lost exception occurred while trying to abandon a message, you may consider to increase the lock duration or reduce the batch size, the exception was {ex.Message}", ex);
             }
             catch (MessagingException ex)
             {
                 // There is nothing we can do as the connection may have been lost, or the underlying queue may have been removed.
                 // If Abandon() fails with this exception, the only recourse is to receive another message.
-                Log.Warn($"A messaging exception occured while trying to abandon a message, this might imply that the connection was lost or the underlying queue got removed, the exception was {ex.Message}", ex);
+                Log.Warn($"A messaging exception occurred while trying to abandon a message, this might imply that the connection was lost or the underlying queue got removed, the exception was {ex.Message}", ex);
             }
             catch (ObjectDisposedException ex)
             {
                 // There is nothing we can do as the object has already been disposed elsewhere
-                Log.Warn($"An object disposed exception occured while trying to abandon a message, this might imply that the connection was lost or the underlying queue got removed, the exception was {ex.Message}", ex);
+                Log.Warn($"An object disposed exception occurred while trying to abandon a message, this might imply that the connection was lost or the underlying queue got removed, the exception was {ex.Message}", ex);
             }
             catch (TransactionException ex)
             {
                 // ASB Sdk beat us to it
-                Log.Warn($"A transaction exception occured while trying to abandon a message, this probably means that the Azure ServiceBus SDK has rolled back the transaction already, the exception was {ex.Message}", ex);
+                Log.Warn($"A transaction exception occurred while trying to abandon a message, this probably means that the Azure ServiceBus SDK has rolled back the transaction already, the exception was {ex.Message}", ex);
             }
             catch (TimeoutException ex)
             {
                 // took to long
-                Log.Warn($"A timeout exception occured while trying to abandon a message, the exception was {ex.Message}", ex);
+                Log.Warn($"A timeout exception occurred while trying to abandon a message, the exception was {ex.Message}", ex);
             }
             return false;
         }
