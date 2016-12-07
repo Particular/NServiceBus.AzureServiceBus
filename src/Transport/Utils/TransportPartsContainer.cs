120ce5acdef983744b9327e931bec2283e7bd378
danielmarbach <daniel.marbach@openplace.net>
2016-12-07 16:08:31 +0100

    Container can call internal ctors

--- src/Transport/Utils/TransportPartsContainer.cs
+++ src/Transport/Utils/TransportPartsContainer.cs
@@ -132,7 +132,7 @@ namespace NServiceBus.Transport.AzureServiceBus
             {
                 var args = constructor.GetParameters().Select(p => Resolve(p.ParameterType)).ToArray();
 
-                return Activator.CreateInstance(type, args, null);
+                return Activator.CreateInstance(type, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance, args);
             };
 
         }
