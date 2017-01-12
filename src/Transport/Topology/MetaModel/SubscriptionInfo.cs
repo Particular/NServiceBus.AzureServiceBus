2f648392c9b6557b117335ac81b1f76d22be6b3c
Sean Feldman <SeanFeldman@users.noreply.github.com>
2017-01-12 17:28:04 +0000

    Events hierarchy from different publishers with EndpointOrientedTopology not handled (#463) (#475)
    * EntityInfo equality needs to have different equality for subscriptions in order to create the necessary receivers in cases where you have an event which is published by multiple endpoints
    
    Topology section manager should choose for subscription the target which matches in path and Namespace
    
    * Test can be enabled again
    
    * Keep SubscriptionIfo comparison out of EntityInfo (#465)
    
    * moving SubscriptionInfo specific comparison out of EntityInfo
    
    * need to consume messages at a higher rate
    
    * switching from internal method to standard Equals and GetHashCode implementation
    
    * API changes approved as not breaking change
    
    # Conflicts:
    #	src/Tests/API/APIApprovals.ApproveAzureServiceBusTransport.approved.txt
    #	src/Transport/Topology/MetaModel/SubscriptionInfo.cs
    #	src/Transport/Topology/Topologies/EndpointOrientedTopologySectionManager.cs

--- src/Transport/Topology/MetaModel/SubscriptionInfo.cs
+++ src/Transport/Topology/MetaModel/SubscriptionInfo.cs
@@ -1,5 +1,7 @@
 ﻿namespace NServiceBus.Transport.AzureServiceBus
 {
+    using System.Linq;
+
     class SubscriptionInfoInternal : EntityInfoInternal
     {
         public IBrokerSideSubscriptionFilterInternal BrokerSideFilter { get; set; }
@@ -7,5 +9,35 @@
         public IClientSideSubscriptionFilterInternal ClientSideFilter { get; set; }
 
         public SubscriptionMetadataInternal Metadata { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (!base.Equals(obj))
+                return false;
+
+            var other = obj as SubscriptionInfoInternal;
+            if (other == null)
+            {
+                return false;
+            }
+
+            var entity = RelationShips.First(r => r.Type == EntityRelationShipTypeInternal.Subscription);
+            var otherEntity = other.RelationShips.First(r => r.Type == EntityRelationShipTypeInternal.Subscription);
+            var targetPathEquals = string.Equals(entity.Target.Path, otherEntity.Target.Path);
+
+            // both target the same topic
+            return targetPathEquals;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = base.GetHashCode();
+                var entity = RelationShips.First(r => r.Type == EntityRelationShipTypeInternal.Subscription);
+                hashCode = (hashCode * 397) ^ entity.Target.Path.GetHashCode();
+                return hashCode;
+            }
+        }
     }
 }
