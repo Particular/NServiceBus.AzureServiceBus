e98282f8120619236c7eaacdc94e6a4a0ce2b2b6
Sean Feldman <feldman.sean@gmail.com>
2017-03-27 14:25:44 -0600

    switch all connection strings to use EnvironmentHelper provided by the core ATT FW

--- src/AcceptanceTests/TransportEncoding/When_receiving_a_message_with_unknown_transport_encoding.cs
+++ src/AcceptanceTests/TransportEncoding/When_receiving_a_message_with_unknown_transport_encoding.cs
@@ -11,6 +11,7 @@
     using NServiceBus.AcceptanceTests.EndpointTemplates;
     using Transport.AzureServiceBus;
     using MessageMutator;
+    using NServiceBus.AcceptanceTests.ScenarioDescriptors;
     using NUnit.Framework;
 
     public class When_receiving_a_message_with_unknown_transport_encoding : NServiceBusAcceptanceTest
@@ -41,7 +42,7 @@
                 // delay raw receive from the DLQ to allow endpoint (queue) creation
                 await Task.Delay(TimeSpan.FromSeconds(10));
 
-                var connectionString = Environment.GetEnvironmentVariable("AzureServiceBusTransport.ConnectionString");
+                var connectionString = EnvironmentHelper.GetEnvironmentVariable("AzureServiceBusTransport.ConnectionString");
 #pragma warning disable 618
                 var namespaceManager = new NamespaceManagerAdapter(NamespaceManager.CreateFromConnectionString(connectionString));
 #pragma warning restore 618
