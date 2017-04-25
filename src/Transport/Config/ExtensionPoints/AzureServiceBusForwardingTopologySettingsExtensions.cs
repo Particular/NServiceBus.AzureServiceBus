49332eb936663945a74a071fb236746cb2765ff4
Sean Feldman <feldman.sean@gmail.com>
2017-04-25 07:59:12 -0600

    Fixing obsolete messages

--- src/Transport/Config/ExtensionPoints/AzureServiceBusForwardingTopologySettingsExtensions.cs
+++ src/Transport/Config/ExtensionPoints/AzureServiceBusForwardingTopologySettingsExtensions.cs
@@ -6,8 +6,16 @@
     [ObsoleteEx(Message = ObsoleteMessages.WillBeInternalized, TreatAsErrorFromVersion = "8.0", RemoveInVersion = "9.0")]
     public static class AzureServiceBusForwardingTopologySettingsExtensions
     {
-        [ObsoleteEx(Message = ObsoleteMessages.WillBeInternalized, TreatAsErrorFromVersion = "8.0", RemoveInVersion = "9.0")]
+        [ObsoleteEx(Message = ObsoleteMessages.NumberOfTopicsInTheBundleWillBeRemoved, TreatAsErrorFromVersion = "8.0", RemoveInVersion = "9.0")]
         public static AzureServiceBusTopologySettings<ForwardingTopology> NumberOfEntitiesInBundle(this AzureServiceBusTopologySettings<ForwardingTopology> topologySettings, int number)
+        {
+            var settings = topologySettings.GetSettings();
+            settings.Set(WellKnownConfigurationKeys.Topology.Bundling.NumberOfEntitiesInBundle, number);
+            return topologySettings;
+        }
+
+        [ObsoleteEx(Message = ObsoleteMessages.NumberOfTopicsInTheBundleWillBeRemoved, TreatAsErrorFromVersion = "8.0", RemoveInVersion = "9.0")]
+        public static AzureServiceBusForwardingTopologySettings NumberOfEntitiesInBundle(this AzureServiceBusForwardingTopologySettings topologySettings, int number)
         {
             var settings = topologySettings.GetSettings();
             settings.Set(WellKnownConfigurationKeys.Topology.Bundling.NumberOfEntitiesInBundle, number);
@@ -22,15 +30,7 @@
             return topologySettings;
         }
 
-        [ObsoleteEx(Message = "Number of topics in the bundle by default is 2. This setting will be removed in the next major version and number of topics used will be 1.", TreatAsErrorFromVersion = "8.0", RemoveInVersion = "9.0")]
-        public static AzureServiceBusForwardingTopologySettings NumberOfEntitiesInBundle(this AzureServiceBusForwardingTopologySettings topologySettings, int number)
-        {
-            var settings = topologySettings.GetSettings();
-            settings.Set(WellKnownConfigurationKeys.Topology.Bundling.NumberOfEntitiesInBundle, number);
-            return topologySettings;
-        }
-
-        [ObsoleteEx(Message = "Bundle prefix will be replaced in the next major version.", TreatAsErrorFromVersion = "8.0", RemoveInVersion = "9.0")]
+        [ObsoleteEx(Message = ObsoleteMessages.WillBeInternalized, TreatAsErrorFromVersion = "8.0", RemoveInVersion = "9.0")]
         public static AzureServiceBusForwardingTopologySettings BundlePrefix(this AzureServiceBusForwardingTopologySettings topologySettings, string prefix)
         {
             var settings = topologySettings.GetSettings();
