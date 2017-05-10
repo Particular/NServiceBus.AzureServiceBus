bc7db8d24638787330b4af2875619b630341edc5
danielmarbach <daniel.marbach@openplace.net>
2017-05-10 12:19:30 +0200

    Bypass topology creation when no manage rights provided Assert manage rights in installer
    Removed unneeded ctor overload for AsyncLazy

--- src/Tests/PreStartupChecks/When_execute_manage_rights_startup_check.cs
+++ src/Tests/PreStartupChecks/When_execute_manage_rights_startup_check.cs
@@ -13,7 +13,7 @@
     public class When_execute_manage_rights_startup_check
     {
         [Test]
-        public async Task Should_return_success_if_all_namespaces_have_manage_rights()
+        public async Task Should_return_no_namespaces_when_all_namespaces_have_manage_rights()
         {
             var settings = new SettingsHolder();
 
@@ -29,14 +29,13 @@
             var manageNamespaceLifeCycle = A.Fake<IManageNamespaceManagerLifeCycle>();
             A.CallTo(() => manageNamespaceLifeCycle.Get(A<string>.Ignored)).Returns(namespaceManager);
 
-            var check = new ManageRightsCheck(manageNamespaceLifeCycle, settings);
-            var result = await check.Run();
+            var result = await ManageRightsCheck.Run(manageNamespaceLifeCycle, settings);
 
-            Assert.True(result.Succeeded);
+            CollectionAssert.IsEmpty(result);
         }
 
         [Test]
-        public async Task Should_return_failure_if_a_namespace_has_not_manage_rights()
+        public async Task Should_namespaces_that_dont_have_manage_rights()
         {
             var settings = new SettingsHolder();
 
@@ -55,40 +54,10 @@
             A.CallTo(() => manageNamespaceLifeCycle.Get("name1")).Returns(trueNamespaceManager);
             A.CallTo(() => manageNamespaceLifeCycle.Get("name2")).Returns(falseNamespaceManager);
 
-            var check = new ManageRightsCheck(manageNamespaceLifeCycle, settings);
-            var result = await check.Run();
+            var result = await ManageRightsCheck.Run(manageNamespaceLifeCycle, settings);
 
-            Assert.False(result.Succeeded);
-        }
-
-        [Test]
-        public async Task Should_compose_right_error_message_when_failed()
-        {
-            var settings = new SettingsHolder();
-
-            var namespaces = new NamespaceConfigurations
-            {
-                {"name1", ConnectionStringValue.Build("namespace1"), NamespacePurpose.Partitioning},
-                {"name2", ConnectionStringValue.Build("namespace2"), NamespacePurpose.Partitioning},
-                {"name3", ConnectionStringValue.Build("namespace3"), NamespacePurpose.Partitioning},
-            };
-            settings.Set(WellKnownConfigurationKeys.Topology.Addressing.Namespaces, namespaces);
-
-            var trueNamespaceManager = A.Fake<INamespaceManager>();
-            A.CallTo(() => trueNamespaceManager.CanManageEntities()).Returns(Task.FromResult(true));
-            var falseNamespaceManager = A.Fake<INamespaceManager>();
-            A.CallTo(() => falseNamespaceManager.CanManageEntities()).Returns(Task.FromResult(false));
-            var manageNamespaceLifeCycle = A.Fake<IManageNamespaceManagerLifeCycle>();
-            A.CallTo(() => manageNamespaceLifeCycle.Get("name1")).Returns(trueNamespaceManager);
-            A.CallTo(() => manageNamespaceLifeCycle.Get("name2")).Returns(falseNamespaceManager);
-            A.CallTo(() => manageNamespaceLifeCycle.Get("name3")).Returns(falseNamespaceManager);
-
-            var check = new ManageRightsCheck(manageNamespaceLifeCycle, settings);
-            var result = await check.Run();
-
-            StringAssert.DoesNotContain("name1", result.ErrorMessage);
-            StringAssert.Contains("name2", result.ErrorMessage);
-            StringAssert.Contains("name3", result.ErrorMessage);
+            CollectionAssert.Contains(result, "name2");
+            CollectionAssert.DoesNotContain(result, "name1");
         }
     }
 }
