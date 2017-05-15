f36d7d243536e780fb6d714966853dd6ae05f93f
danielmarbach <daniel.marbach@openplace.net>
2017-05-15 11:25:49 +0200

    Removed unneeded ctor overload for AsyncLazy

--- src/Transport/Utils/AsyncLazy.cs
+++ src/Transport/Utils/AsyncLazy.cs
@@ -6,11 +6,6 @@
 
     class AsyncLazy<T> : Lazy<Task<T>>
     {
-        public AsyncLazy(Func<T> valueFactory) :
-            base(() => Task.Run(valueFactory))
-        {
-        }
-
         public AsyncLazy(Func<Task<T>> taskFactory) :
             base(() => Task.Run(taskFactory))
         {
