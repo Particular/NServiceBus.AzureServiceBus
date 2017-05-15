565f762c3156cec698adc627742d040b56f6cf02
danielmarbach <daniel.marbach@openplace.net>
2017-05-15 15:43:43 +0200

    Approve API and remove unnecessary using

--- src/Transport/Topology/Topologies/ForwardingTopology.cs
+++ src/Transport/Topology/Topologies/ForwardingTopology.cs
@@ -3,7 +3,6 @@ namespace NServiceBus
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Reflection;
     using System.Threading.Tasks;
     using Logging;
     using Routing;
