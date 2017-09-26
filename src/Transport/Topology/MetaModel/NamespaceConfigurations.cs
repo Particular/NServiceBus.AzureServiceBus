25ab39115ed4f371fbd70aa8a31af2fcd1d2801e
danielmarbach <daniel.marbach@openplace.net>
2017-09-26 10:45:39 +0200

    RegisteredEndpoints can be added to the namespace info

--- src/Transport/Topology/MetaModel/NamespaceConfigurations.cs
+++ src/Transport/Topology/MetaModel/NamespaceConfigurations.cs
@@ -30,24 +30,25 @@
             return GetEnumerator();
         }
 
-        public void Add(string alias, string connectionString, NamespacePurpose purpose)
+        public NamespaceInfo Add(string alias, string connectionString, NamespacePurpose purpose)
         {
             var definition = new NamespaceInfo(alias, connectionString, purpose);
-
             var namespaceInfo = inner.SingleOrDefault(x => x.Connection == definition.Connection);
             if (namespaceInfo != null)
             {
                 Log.Info($"Duplicated connection string for namespace `{namespaceInfo.Alias}` and alias `{alias}.`  + {Environment.NewLine} + `{alias}` namespace alias was not registered.");
-                return;
+                return namespaceInfo;
             }
 
-            if (inner.Any(x => string.Equals(x.Alias, alias, StringComparison.OrdinalIgnoreCase)))
+            namespaceInfo = inner.SingleOrDefault(x => string.Equals(x.Alias, alias, StringComparison.OrdinalIgnoreCase));
+            if (namespaceInfo != null)
             {
                 Log.Info($"Duplicated namespace alias `{alias}` configuration detected. Registered only once");
-                return;
+                return namespaceInfo;
             }
 
             inner.Add(definition);
+            return definition;
         }
 
         public string GetConnectionString(string name)
