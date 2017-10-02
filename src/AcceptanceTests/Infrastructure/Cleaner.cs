9fcd1e139e19bc33c829fc5c382aabfa9c5f2518
Sean Feldman <feldman.sean@gmail.com>
2017-10-01 21:46:59 -0600

    cleanup test to remove all queues and topics

---
+++ src/AcceptanceTests/Infrastructure/Cleaner.cs
@@ -0,0 +1,28 @@
+﻿namespace NServiceBus.AzureServiceBus.AcceptanceTests.Infrastructure
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.ServiceBus;
+    using NUnit.Framework;
+
+    public class Cleaner
+    {
+        [Test, Explicit("Intended to be executed explicitly to delete all queues and topics.")]
+        [Category("Cleanup")]
+        public async Task DeleteEntities()
+        {
+            var namespaceManager = NamespaceManager.CreateFromConnectionString(TestUtility.GetDefaultConnectionString());
+
+            var queryQueues = namespaceManager.GetQueuesAsync();
+            var queryTopics = namespaceManager.GetTopicsAsync();
+            await Task.WhenAll(queryQueues, queryTopics)
+                .ConfigureAwait(false);
+
+            var deleteQueues = queryQueues.Result.Select(queueDescription => namespaceManager.DeleteQueueAsync(queueDescription.Path));
+            var deleteTopics = queryTopics.Result.Select(topicDescription => namespaceManager.DeleteTopicAsync(topicDescription.Path));
+
+            await Task.WhenAll(deleteQueues.Concat(deleteTopics))
+                .ConfigureAwait(false);
+        }
+    }
+}
---
+++ src/AcceptanceTests/Infrastructure/TestUtility.cs
@@ -0,0 +1,19 @@
+﻿namespace NServiceBus.AzureServiceBus.AcceptanceTests.Infrastructure
+{
+    using System;
+    using NServiceBus.AcceptanceTests.ScenarioDescriptors;
+
+    static class TestUtility
+    {
+        public static string GetDefaultConnectionString()
+        {
+            var environmentVariableName = $"{nameof(AzureServiceBusTransport)}.ConnectionString";
+            var connectionString = EnvironmentHelper.GetEnvironmentVariable(environmentVariableName);
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new Exception($"Oh no! Could not find an environment variable '{environmentVariableName}'.");
+            }
+            return connectionString;
+        }
+    }
+}
