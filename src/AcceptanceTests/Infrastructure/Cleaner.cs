30e170a5d55467437a959eaeb33625edd60dbe6d
Sean Feldman <feldman.sean@gmail.com>
2017-10-01 22:54:03 -0600

    Adding retries and preventing Cleaner from throwing and aborting the build

--- src/AcceptanceTests/Infrastructure/Cleaner.cs
+++ src/AcceptanceTests/Infrastructure/Cleaner.cs
@@ -1,8 +1,11 @@
 ﻿namespace NServiceBus.AzureServiceBus.AcceptanceTests.Infrastructure
 {
+    using System;
     using System.Linq;
     using System.Threading.Tasks;
+    using Logging;
     using Microsoft.ServiceBus;
+    using Microsoft.ServiceBus.Messaging;
     using NServiceBus.AcceptanceTests;
     using NUnit.Framework;
 
@@ -22,8 +25,32 @@
             var deleteQueues = queryQueues.Result.Select(queueDescription => namespaceManager.DeleteQueueAsync(queueDescription.Path));
             var deleteTopics = queryTopics.Result.Select(topicDescription => namespaceManager.DeleteTopicAsync(topicDescription.Path));
 
-            await Task.WhenAll(deleteQueues.Concat(deleteTopics))
+            await TryWithRetries(Task.WhenAll(deleteQueues.Concat(deleteTopics)), 5)
                 .ConfigureAwait(false);
         }
+
+        static async Task TryWithRetries(Task task, int maxRetryAttempts, int usedRetryAttempts = 0)
+        {
+            try
+            {
+                await task
+                    .ConfigureAwait(false);
+            }
+            catch (Exception exception)
+                when (usedRetryAttempts < maxRetryAttempts && (exception is TimeoutException || exception is MessagingCommunicationException || exception is ServerBusyException))
+            {
+                logger.InfoFormat("Attempt to delete all queues and topics has failed. Trying attempt {0}/{1} in 5 seconds.", usedRetryAttempts + 2, maxRetryAttempts);
+                await Task.Delay(5000)
+                    .ConfigureAwait(false);
+                await TryWithRetries(task, maxRetryAttempts, usedRetryAttempts + 1)
+                    .ConfigureAwait(false);
+            }
+            catch (Exception exception)
+            {
+                logger.InfoFormat($"Failed to delete all queues and topics after {0}. Last received exception:\n{1}", usedRetryAttempts, exception.Message);
+            }
+        }
+
+        static ILog logger = LogManager.GetLogger(typeof(Cleaner));
     }
 }
