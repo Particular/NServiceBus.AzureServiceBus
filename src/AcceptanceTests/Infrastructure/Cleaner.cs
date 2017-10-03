5cd2da49fe63ef0daf3a5f91c75cb8dd08b7f13f
Sean Feldman <SeanFeldman@users.noreply.github.com>
2017-10-03 00:54:38 -0600

    Cleanup test to remove all queues and topics (#635)
    * cleanup test to remove all queues and topics
    
    * all tests under ATTs must inherit from NServiceBusAcceptanceTest
    
    * Adding retries and preventing Cleaner from throwing and aborting the build
    
    * retries on the entity level
    
    * cleaner query result invocation
    
    * replace ILogger with NUnit's TestContext to capture logs properly.
    
    * simplified exception filter
    
    * Do not attempt to remove both types of entities in simultaneously
    
    * Deleting fallback namespace entities as well, connection string property instead of method

---
+++ src/AcceptanceTests/Infrastructure/Cleaner.cs
@@ -0,0 +1,64 @@
+﻿namespace NServiceBus.AzureServiceBus.AcceptanceTests.Infrastructure
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.ServiceBus;
+    using NServiceBus.AcceptanceTests;
+    using NUnit.Framework;
+    using Utils;
+
+    public class Cleaner : NServiceBusAcceptanceTest
+    {
+        [Test, Explicit("Intended to be executed explicitly to delete all queues and topics.")]
+        [Category("Cleanup")]
+        public async Task DeleteEntities()
+        {
+            var namespaceManager = NamespaceManager.CreateFromConnectionString(TestUtility.DefaultConnectionString);
+            await DeleteEntities(namespaceManager);
+
+            namespaceManager = NamespaceManager.CreateFromConnectionString(TestUtility.FallbackConnectionString);
+            await DeleteEntities(namespaceManager);
+        }
+
+        static async Task DeleteEntities(NamespaceManager namespaceManager)
+        {
+            var queryQueues = namespaceManager.GetQueuesAsync();
+            var queryTopics = namespaceManager.GetTopicsAsync();
+            await Task.WhenAll(queryQueues, queryTopics)
+                .ConfigureAwait(false);
+
+            const int maxRetryAttempts = 5;
+            var deleteQueues = (await queryQueues).Select(queueDescription => TryWithRetries(queueDescription.Path, namespaceManager.DeleteQueueAsync(queueDescription.Path), maxRetryAttempts));
+
+            await Task.WhenAll(deleteQueues)
+                .ConfigureAwait(false);
+
+            var deleteTopics = (await queryTopics).Select(topicDescription => TryWithRetries(topicDescription.Path, namespaceManager.DeleteTopicAsync(topicDescription.Path), maxRetryAttempts));
+            await Task.WhenAll(deleteTopics)
+                .ConfigureAwait(false);
+        }
+
+        static async Task TryWithRetries(string entityPath, Task task, int maxRetryAttempts, int usedRetryAttempts = 0)
+        {
+            try
+            {
+                await task
+                    .ConfigureAwait(false);
+            }
+            catch (Exception exception)
+                when (usedRetryAttempts < maxRetryAttempts && (exception is TimeoutException || exception.IsTransientException()))
+            {
+                TestContext.WriteLine($"Attempt to delete '{entityPath}' has failed. Retrying attempt {usedRetryAttempts + 1}/{maxRetryAttempts} in 5 seconds.");
+                await Task.Delay(5000)
+                    .ConfigureAwait(false);
+                await TryWithRetries(entityPath, task, maxRetryAttempts, usedRetryAttempts + 1)
+                    .ConfigureAwait(false);
+            }
+            catch (Exception exception)
+            {
+                TestContext.WriteLine($"Failed to delete '{entityPath}' after {usedRetryAttempts}. Last received exception:\n{exception.Message}");
+            }
+        }
+    }
+}
---
+++ src/AcceptanceTests/Infrastructure/TestUtility.cs
@@ -0,0 +1,45 @@
+﻿namespace NServiceBus.AzureServiceBus.AcceptanceTests.Infrastructure
+{
+    using System;
+    using NServiceBus.AcceptanceTests.ScenarioDescriptors;
+
+    static class TestUtility
+    {
+        static string connectionString;
+        static string fallbackConnectionString;
+
+        public static string DefaultConnectionString
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(connectionString))
+                {
+                    var environmentVariableName = $"{nameof(AzureServiceBusTransport)}.ConnectionString";
+                    connectionString = EnvironmentHelper.GetEnvironmentVariable(environmentVariableName);
+                    if (string.IsNullOrEmpty(connectionString))
+                    {
+                        throw new Exception($"Oh no! Could not find an environment variable '{environmentVariableName}'.");
+                    }
+                }
+                return connectionString;
+            }
+        }
+
+        public static string FallbackConnectionString
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(fallbackConnectionString))
+                {
+                    var environmentVariableName = $"{nameof(AzureServiceBusTransport)}.ConnectionString";
+                    fallbackConnectionString = EnvironmentHelper.GetEnvironmentVariable(environmentVariableName);
+                    if (string.IsNullOrEmpty(fallbackConnectionString))
+                    {
+                        throw new Exception($"Oh no! Could not find an environment variable '{environmentVariableName}'.");
+                    }
+                }
+                return fallbackConnectionString;
+            }
+        }
+    }
+}
