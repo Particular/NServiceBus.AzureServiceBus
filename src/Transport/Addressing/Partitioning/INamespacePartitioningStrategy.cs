d622a921b2c1088a38cfb510aed83c8f5d0d4011
Sean Feldman <feldman.sean@gmail.com>
2017-10-13 13:59:13 -0600

    INamespacePartitioningStrategy xml doco

--- src/Transport/Addressing/Partitioning/INamespacePartitioningStrategy.cs
+++ src/Transport/Addressing/Partitioning/INamespacePartitioningStrategy.cs
@@ -2,8 +2,14 @@
 {
     using System.Collections.Generic;
 
+    /// <summary>
+    /// Contract to implement custom namespace partitioning.
+    /// </summary>
     public interface INamespacePartitioningStrategy
     {
+        /// <summary>
+        /// Return a set of namespaces required by strategy for <see cref="PartitioningIntent"/>.
+        /// </summary>
         IEnumerable<RuntimeNamespaceInfo> GetNamespaces(PartitioningIntent partitioningIntent);
     }
 }
