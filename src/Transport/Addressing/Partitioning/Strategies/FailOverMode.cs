6db0e699df14a3a611cf067ac25d0a36abe6d522
Sean Feldman <feldman.sean@gmail.com>
2017-10-13 14:05:09 -0600

    FailOverNamespacePartitioning related xml doco

--- src/Transport/Addressing/Partitioning/Strategies/FailOverMode.cs
+++ src/Transport/Addressing/Partitioning/Strategies/FailOverMode.cs
@@ -1,8 +1,13 @@
 ﻿namespace NServiceBus.Transport.AzureServiceBus
 {
+    /// <summary>
+    /// Namespace in use for <see cref="FailOverNamespacePartitioning"/>.
+    /// </summary>
     public enum FailOverMode
     {
+        /// <summary></summary>
         Primary,
+        /// <summary></summary>
         Secondary
     }
 }
