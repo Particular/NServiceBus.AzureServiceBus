5f1cc4ee5dff2e64354d7e4c4c7fcd0196fade49
yvesgoeleven <yves@goeleven.com>
2017-10-27 14:35:29 +0200

    read the test title yves

--- src/Tests/When_determining_subscription_name.cs
+++ src/Tests/When_determining_subscription_name.cs
@@ -15,7 +15,7 @@
             var subscriptionName = NamingConventions.SubscriptionNamingConvention(null, null, endpointName);
             Guid result;
             Assert.AreEqual(shouldBeConvertedToGuid, Guid.TryParse(subscriptionName, out result));
-            Assert.AreNotEqual(result, default(Guid));
+            Assert.AreEqual(result, default(Guid));
         }
     }
 }
