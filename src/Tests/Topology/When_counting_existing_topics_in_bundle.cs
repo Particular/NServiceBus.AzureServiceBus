e3517fda64660eb06001bb40361090c30f9d4e1a
Sean Feldman <feldman.sean@gmail.com>
2017-11-21 23:18:18 -0700

    fix #683 count topics in bundle with numeric values only
    # Conflicts:
    #	src/Transport/Topology/NumberOfTopicsInBundleCheck.cs

---
+++ src/Tests/Topology/When_counting_existing_topics_in_bundle.cs
@@ -0,0 +1,53 @@
+﻿namespace NServiceBus.Azure.WindowsAzureServiceBus.Tests.Topology
+{
+    using System;
+    using System.Text.RegularExpressions;
+    using System.Threading.Tasks;
+    using Microsoft.ServiceBus;
+    using Microsoft.ServiceBus.Messaging;
+    using NUnit.Framework;
+    using TestUtils;
+    using Transport.AzureServiceBus;
+
+    [TestFixture]
+    [Category("AzureServiceBus")]
+    public class When_counting_existing_topics_in_bundle
+    {
+#pragma warning disable 618
+        NamespaceManagerAdapter namespaceManager;
+#pragma warning restore 618
+        string nonBundledTopic;
+
+        [Test]
+        public async Task Should_only_count_topics_following_bundle_pattern()
+        {
+            var bundlePrefix = $"bundle{DateTime.Now.Ticks}-";
+            nonBundledTopic = $"{bundlePrefix}x";
+
+#pragma warning disable 618
+            namespaceManager = new NamespaceManagerAdapter(NamespaceManager.CreateFromConnectionString(AzureServiceBusConnectionString.Value));
+#pragma warning restore 618
+
+            try
+            {
+                await namespaceManager.CreateTopic(new TopicDescription(nonBundledTopic));
+            }
+            catch (MessagingEntityAlreadyExistsException)
+            {
+                // ignore if topic already exists
+            }
+
+            var filter = $"startswith(path, '{bundlePrefix}') eq true";
+            var foundTopics = await namespaceManager.GetTopics(filter).ConfigureAwait(false);
+
+            var topicsInBundle = NumberOfTopicsInBundleCheck.CountTopicsInBundle(new Regex($@"^{bundlePrefix}\d+$", RegexOptions.CultureInvariant), foundTopics);
+            Assert.AreEqual(0, topicsInBundle);
+        }
+
+        [TearDown]
+        public Task TearDown()
+        {
+            return namespaceManager.DeleteTopic(nonBundledTopic);
+        }
+    }
+}
--- src/Transport/Topology/NumberOfTopicsInBundleCheck.cs
+++ src/Transport/Topology/NumberOfTopicsInBundleCheck.cs
@@ -1,7 +1,10 @@
 ﻿namespace NServiceBus.Transport.AzureServiceBus
 {
+    using System.Collections.Generic;
     using System.Linq;
+    using System.Text.RegularExpressions;
     using System.Threading.Tasks;
+    using Microsoft.ServiceBus.Messaging;
     using NServiceBus.AzureServiceBus.Topology.MetaModel;
 
     static class NumberOfTopicsInBundleCheck
@@ -9,6 +12,7 @@
         public static async Task<NamespaceBundleConfigurations> Run(IManageNamespaceManagerLifeCycle manageNamespaceManagerLifeCycle, NamespaceConfigurations namespaceConfigurations, string bundlePrefix)
         {
             var namespaceBundleConfigurations = new NamespaceBundleConfigurations();
+            var topicInBundleNameRegex = new Regex($@"^{bundlePrefix}\d+$", RegexOptions.CultureInvariant);
 
             foreach (var namespaceConfiguration in namespaceConfigurations)
             {
@@ -26,12 +30,17 @@
                 {
                     var filter = $"startswith(path, '{bundlePrefix}') eq true";
                     var foundTopics = await namespaceManagerThatCanQueryAndFilterTopics.GetTopics(filter).ConfigureAwait(false);
-                    numberOfTopics = foundTopics.Count();
+                    numberOfTopics = CountTopicsInBundle(topicInBundleNameRegex, foundTopics);
                 }
                 namespaceBundleConfigurations.Add(namespaceConfiguration.Alias, numberOfTopics);
             }
 
             return namespaceBundleConfigurations;
+        }
+
+        public static int CountTopicsInBundle(Regex topicInBundleNameRegex, IEnumerable<TopicDescription> topics)
+        {
+            return topics.Count(topic => topicInBundleNameRegex.IsMatch(topic.Path));
         }
     }
 }
