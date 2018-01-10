14a61490dfd870d45148c3032855b678fa6430e6
Sean Feldman <feldman.sean@gmail.com>
2018-01-09 23:21:00 -0700

    cache should take into consideration entity type. For different entity types with the same value different results can be returned

--- src/Transport/Addressing/AddressingLogic.cs
+++ src/Transport/Addressing/AddressingLogic.cs
@@ -1,5 +1,6 @@
 ﻿namespace NServiceBus.Transport.AzureServiceBus
 {
+    using System;
     using System.Collections.Concurrent;
 
     class AddressingLogic
@@ -18,19 +19,57 @@
                 return address;
             }
 
-            var path = cachedPaths.GetOrAdd(value, x =>
+            string pathOrName;
+            switch (entityType)
             {
-                var newPath = composition.GetEntityPath(x, entityType);
-                newPath = sanitizationStrategy.Sanitize(newPath, entityType);
-                cachedPaths.TryAdd(newPath, newPath);
-                return newPath;
-            });
+                case EntityType.Queue:
+                    pathOrName = cachedQueuePaths.GetOrAdd(value, x =>
+                    {
+                        var newPath = composition.GetEntityPath(x, entityType);
+                        newPath = sanitizationStrategy.Sanitize(newPath, entityType);
+                        cachedQueuePaths.TryAdd(newPath, newPath);
+                        return newPath;
+                    });
+                    break;
+                case EntityType.Topic:
+                    pathOrName = cachedTopicPaths.GetOrAdd(value, x =>
+                    {
+                        var newPath = composition.GetEntityPath(x, entityType);
+                        newPath = sanitizationStrategy.Sanitize(newPath, entityType);
+                        cachedTopicPaths.TryAdd(newPath, newPath);
+                        return newPath;
+                    });
+                    break;
+                case EntityType.Subscription:
+                    pathOrName = cachedSubscriptionName.GetOrAdd(value, x =>
+                    {
+                        var newPath = composition.GetEntityPath(x, entityType);
+                        newPath = sanitizationStrategy.Sanitize(newPath, entityType);
+                        cachedSubscriptionName.TryAdd(newPath, newPath);
+                        return newPath;
+                    });
+                    break;
+                case EntityType.Rule:
+                    pathOrName = cachedRuleName.GetOrAdd(value, x =>
+                    {
+                        var newPath = composition.GetEntityPath(x, entityType);
+                        newPath = sanitizationStrategy.Sanitize(newPath, entityType);
+                        cachedRuleName.TryAdd(newPath, newPath);
+                        return newPath;
+                    });
+                    break;
+                default:
+                    throw new Exception($"Unexpected entity type '{entityType}'.");
+            }
 
-            return new EntityAddress(path, address.Suffix);
+            return new EntityAddress(pathOrName, address.Suffix);
         }
 
         readonly ICompositionStrategy composition;
         readonly ISanitizationStrategy sanitizationStrategy;
-        ConcurrentDictionary<string, string> cachedPaths = new ConcurrentDictionary<string, string>();
+        ConcurrentDictionary<string, string> cachedQueuePaths = new ConcurrentDictionary<string, string>();
+        ConcurrentDictionary<string, string> cachedTopicPaths = new ConcurrentDictionary<string, string>();
+        ConcurrentDictionary<string, string> cachedSubscriptionName = new ConcurrentDictionary<string, string>();
+        ConcurrentDictionary<string, string> cachedRuleName = new ConcurrentDictionary<string, string>();
     }
 }
