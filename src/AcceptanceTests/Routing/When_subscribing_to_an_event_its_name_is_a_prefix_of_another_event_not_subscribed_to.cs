59f9789e3c30f9169d029c9dc9e157db4536218c
Sean Feldman <feldman.sean@gmail.com>
2018-02-13 00:20:27 -0700

    red test

---
+++ src/AcceptanceTests/Routing/When_subscribing_to_an_event_its_name_is_a_prefix_of_another_event_not_subscribed_to.cs
@@ -0,0 +1,88 @@
+﻿namespace NServiceBus.Azure.Transports.WindowsAzureServiceBus.AcceptanceTests.Routing
+{
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using NServiceBus.AcceptanceTests;
+    using NServiceBus.AcceptanceTests.EndpointTemplates;
+    using NUnit.Framework;
+
+    public class When_subscribing_to_an_event_its_name_is_a_prefix_of_another_event_not_subscribed_to : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public async Task Should_not_subscribe_to_another_event()
+        {
+            var context = await Scenario.Define<Context>()
+                .WithEndpoint<Publisher>(b => b.When(async bus =>
+                {
+                    await bus.Publish<EventSubscriberNotInterestedIn>();
+                }))
+                .WithEndpoint<Subscriber>()
+                .WithEndpoint<ErrorSpy>()
+                .Done(ctx => ctx.ReceivedIncorrectEvent)
+                .Run();
+
+            Assert.That(context.ReceivedIncorrectEvent, Is.False, $"Subscriber should never receive {nameof(EventSubscriberNotInterestedIn)} event, but it did.");
+        }
+
+        public class Context : ScenarioContext
+        {
+            public bool ReceivedIncorrectEvent { get; set; }
+        }
+
+        public class Publisher : EndpointConfigurationBuilder
+        {
+            public Context Context { get; set; }
+
+            public Publisher()
+            {
+                EndpointSetup<DefaultPublisher>();
+            }
+        }
+
+        public class Subscriber : EndpointConfigurationBuilder
+        {
+            public Subscriber()
+            {
+                EndpointSetup<DefaultServer>(c => c.SendFailedMessagesTo("error"));
+            }
+
+            public class MyEventHandler : IHandleMessages<Event>
+            {
+                public Context Context { get; set; }
+
+                public Task Handle(Event message, IMessageHandlerContext context)
+                {
+                    return Task.FromResult(0);
+                }
+            }
+        }
+
+        class ErrorSpy : EndpointConfigurationBuilder
+        {
+            public ErrorSpy()
+            {
+                EndpointSetup<DefaultServer>().CustomEndpointName("error");
+            }
+
+            class Handler : IHandleMessages<EventSubscriberNotInterestedIn>
+            {
+                public Context TestContext { get; set; }
+
+                public Task Handle(EventSubscriberNotInterestedIn message, IMessageHandlerContext context)
+                {
+                    TestContext.ReceivedIncorrectEvent = true;
+                    return Task.FromResult(0);
+                }
+            }
+        }
+
+
+        public interface Event : IEvent
+        {
+        }
+
+        public interface EventSubscriberNotInterestedIn : IEvent
+        {
+        }
+    }
+}
