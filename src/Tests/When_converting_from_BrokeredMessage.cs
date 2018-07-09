d04c36054ea412678f0216810e3c3dfa083fdfc4
Mike Minutillo <mike.minutillo@particular.net>
2018-07-09 13:40:04 +0800

    Add failing test

---
+++ src/Tests/When_converting_from_BrokeredMessage.cs
@@ -0,0 +1,23 @@
+﻿namespace NServiceBus.Azure.WindowsAzureServiceBus.Tests
+{
+    using Microsoft.ServiceBus.Messaging;
+    using NServiceBus.Azure.Transports.WindowsAzureServiceBus;
+    using NUnit.Framework;
+
+    [TestFixture]
+    [Category("Azure")]
+    public class When_converting_from_BrokeredMessage
+    {
+        [Test]
+        public void Should_handle_missing_intent()
+        {
+            var brokeredMessage = new BrokeredMessage();
+            
+            // NOTE: At least property must be set for the brokered message converter to try and retrieve the intent
+            brokeredMessage.Properties["SomeProperty"] = "SomeValue";
+            //brokeredMessage.Properties[Headers.MessageIntent] = MessageIntentEnum.Publish;
+            
+            Assert.DoesNotThrow(() => brokeredMessage.ToTransportMessage());
+        }
+    }
+}
