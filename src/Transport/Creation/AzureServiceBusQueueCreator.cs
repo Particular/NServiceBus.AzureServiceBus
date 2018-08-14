a58627c18ab4f1e6ca47baedbf80701cb707d772
Sean Feldman <feldman.sean@gmail.com>
2018-08-14 14:25:26 -0600

    Assign ForwardDeadLetteredMessagesTo conditionally

--- src/Transport/Creation/AzureServiceBusQueueCreator.cs
+++ src/Transport/Creation/AzureServiceBusQueueCreator.cs
@@ -36,8 +36,12 @@
                 EnablePartitioning = queueSettings.EnablePartitioning,
                 SupportOrdering = queueSettings.SupportOrdering,
                 AutoDeleteOnIdle = queueSettings.AutoDeleteOnIdle,
-                ForwardDeadLetteredMessagesTo = queueSettings.ForwardDeadLetteredMessagesTo
             };
+
+            if (queueSettings.ForwardDeadLetteredMessagesToCondition(queuePath))
+            {
+                description.ForwardDeadLetteredMessagesTo = description.ForwardDeadLetteredMessagesTo;
+            }
 
             queueSettings.DescriptionCustomizer(description);
 
