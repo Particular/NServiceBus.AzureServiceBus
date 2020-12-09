f1fbafe4a047230be3deb001efb9dc77ccba065e
Sean Feldman <SeanFeldman@users.noreply.github.com>
2020-12-09 01:17:51 -0700

    Fix API Approvals (#938)

--- src/Tests/APIApprovals.cs
+++ src/Tests/APIApprovals.cs
@@ -10,7 +10,10 @@
         [Test]
         public void ApproveAzureServiceBusTransport()
         {
-            var publicApi = ApiGenerator.GeneratePublicApi(typeof(AzureServiceBusTransport).Assembly);
+            var publicApi = typeof(AzureServiceBusTransport).Assembly.GeneratePublicApi(new ApiGeneratorOptions
+            {
+                ExcludeAttributes = new[] { "System.Runtime.Versioning.TargetFrameworkAttribute", "System.Reflection.AssemblyMetadataAttribute" }
+            });
             Approver.Verify(publicApi);
         }
     }
